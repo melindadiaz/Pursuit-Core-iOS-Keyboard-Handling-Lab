a3d06df7a318804f0b4ac9d9fd47fd64174919ec
melinda <melindad_82@msn.com>
2020-02-04 15:54:57 -0500

    fixed registered keyboard function, adjusted keyboard to move UI up or down

--- KeyboardHandlingLogINLab/KeyboardHandlingLogINLab/ViewController.swift
+++ KeyboardHandlingLogINLab/KeyboardHandlingLogINLab/ViewController.swift
@@ -19,25 +19,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var fluxxCenterLogoYConstraint: NSLayoutConstraint!
     
     private var keyboardIsVisible = false
+    
     //WHy do we need this if we have the IBOutlet????
     private var originalYConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         registerForKeyboardNotifications()
+        usernameTextField.delegate = self
+        passwordTextfield.delegate = self
+        
     }
     //YOu need this for the keyboard
     override func viewWillAppear(_ animated: Bool) {
+        //DO NOT FORGET THE SUPER!!!!
+        super.viewWillAppear(true)
+        unregisterForKeyboardNotifications()
         registerForKeyboardNotifications()
     }
     
-    //TODO: Why should this be in an extension????
+    
     private func registerForKeyboardNotifications() {
         //we want to listen to keyboard will show. when the keyboard comes upUIResponder will always have the resignFirstResponder(), We have 2 selectors keyboard will show and keyboard will hide. it can pass in data but it could be buggy
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-        
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        //you need this so the keyboard will hide it adds an observer to watch over it
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    //TODO: Why should this be in an extension????
+    private func unregisterForKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+           NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+           
+        
     }
     //TODO: Why UIKeyboardFrameBeginUserInfoKey????????????
     @objc private func keyboardWillShow(_ notification: NSNotification) {
@@ -45,7 +58,7 @@ class ViewController: UIViewController {
             return //this is not a place for fatalError so we will just return
         }
         //this updates the UI and moves the textfield up but it will keep moving the UI up so we need to constrainthat so that it doesnt move off the screen
-        //Move KeyboardUP
+        moveUpKeyboard(keyboardFrame.size.height)
     }
     @objc private func keyboardWillHide(_ notification: NSNotification) {
         resetUI()
@@ -68,3 +81,10 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        resetUI()
+        return true //You must return a Bool in order for it to work
+    }
+}
